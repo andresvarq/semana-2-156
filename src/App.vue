db9099d86f1f4fde7fff7a55e885878113850d3a
andresvarq <66755854+andresvarq@users.noreply.github.com>
2020-12-05 19:06:54 -0500

    intento

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
+    <section-api></section-api>
   </div>
 </template>
 
 <script>
 
+import SectionApi from './components/SectionApi.vue';
+
 export default {
   name: 'App',
+  components: {
+    SectionApi,
+  },
+  data() {
+    return{
+      title: 'Vivo en vue',
+    }
+  }
 }
 </script>
 
---
+++ src/components/SectionApi.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+
+export default {
+    name: 'SectionApi',
+    data() {
+        return {
+            news: null,
+        }
+    },
+    mounted() {
+        fetch('https://newsapi.org/v2/top-headlines?country=co&category=sports&apiKey=8982d839d7174ed0959e518d808770ea', {method: 'GET'})
+        .then(response => {
+            (this.news = response)
+            console.log(this.news)
+        })
+    }
+}
+
+</script>
