fdc38d91bd1e74f4de1b13b5235a7afb386f935b
andresvarq <66755854+andresvarq@users.noreply.github.com>
2020-12-07 11:03:11 -0500

    Desarrollo con vue cli

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,54 @@
+<template>
+<!-- Footer -->
+<footer class="page-footer pt-5" style="background-color: #f3fa53; color: black;">
+    <div class="container-fluid text-center text-md-left">
+        <div class="row">
+            <div class="col-md-6 mt-md-0 mt-3">
+                <p>Porque, para nosotros, el Fútbol, más que pasión por un deporte es pasión por el arte del esférico, consideramos importante resignificarlo a partir de los equipos locales, y lo hacemos, no como periodistas sino como hinchas.</p>
+                <address >
+                    <a href="https://github.com/andresvarq/semana-1-156" style="color: #f51720;">Visítanos</a>
+                </address>
+            </div>
+            <hr class="clearfix w-100 d-md-none pb-3">
+            <div class="col-md-6 mb-md-0 mb-3">
+
+                <h5 class="text-uppercase font-weight-bold">Contactanos</h5>
+                <div>
+                    <p class="contenedor_datos__miembro">Administrador del sitio Web: <br>
+                    Andrés Vargas <br>
+                    <a href="mailto: andres_vargas97@hotmail.com" style="color: #f51720;">andres_vargas97@hotmail.com</a> <br>
+                    Bogotá, Colombia</p>
+                </div>            
+                <div>
+                    <p class="contenedor_datos__miembro">WebMaster: <br>
+                    Mario De La Pava <br>
+                    <a href="mailto: pava092@hotmail.com" style="color: #f51720;">pava092@hotmail.com</a> <br>
+                    Armenia, Colombia </p>
+                </div>            
+                <div>
+                    <p class="contenedor_datos__miembro">Gestora de contenidos: <br>
+                    Amparo González <br>
+                    <a href="mailto: dragonmoreno@gmail.com" style="color: #f51720;">dragonmoreno@gmail.com</a> <br>
+                    Medellín, Colombia</p>
+                </div>
+                <div>
+                    <p> Desarrollo: <br> 
+                    Santiago Muñoz González <br>
+                    <a href="mailto: tiagomg1993@gmail.com" style="color: #f51720;">tiagomg1993@gmail.com</a> <br>
+                    Colombia</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</template>
+
+<script>
+export default {
+    name: 'PageFooter',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/SectionApi.vue
+++ src/components/SectionApi.vue
@@ -1,31 +1,31 @@
 <template>
-    
+    <div>
+
+    </div>
 </template>
 
 <script>
 export default {
     name: 'SectionApi',
     data(){
-        news: null
+        return {
+            news: null
+        }
     },
     mounted(){
-        fetch("https://api-football-v1.p.rapidapi.com/v2/predictions/157462", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "24c5dd9bbamshf3f8ce442013e00p1187a2jsnd442d6f65bda",
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+        fetch("https://api-football-v1.p.rapidapi.com/v2/countries", {
+            "method": "GET", "headers": {
+                "x-rapidapi-key": "24c5dd9bbamshf3f8ce442013e00p1187a2jsnd442d6f65bda",
+                "x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+            }
+        })
+        .then(response => {
+            (this.news = response.json())
+            console.log(this.news);
+        })
+        .catch(err => {
+            console.error(err);
+        });
     }
 }
 </script>
-
-<style scoped>
-
-</style>
