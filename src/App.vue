0efc943ea6b8f9699f55242c5f86ecb8ef262d5d
andresvarq <66755854+andresvarq@users.noreply.github.com>
2020-12-07 00:25:57 -0500

    reinicio app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div id="app">
-    <section-api></section-api>
+
   </div>
 </template>
 
 <script>
 
-import SectionApi from './components/SectionApi.vue';
+
 
 export default {
   name: 'App',
-  components: {
-    SectionApi,
-  },
-  data() {
-    return{
-      title: 'Vivo en vue',
-    }
-  }
+
 }
 </script>
 
