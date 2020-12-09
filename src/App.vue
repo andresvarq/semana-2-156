f0be188370bbabac833aff5bf90e0301f7985aa7
TiagoMg93 <tiagomg1993@gmail.com>
2020-12-09 00:33:17 -0500

    carpeta entregable reto 2

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+
+    <top-menu></top-menu>
+
+    <main-banner></main-banner>
+
+    <div class="container-fluid" id="news">
+    <section-api></section-api>
+    </div>
+
+
+    <div class="container-fluid" id="team">
+    <div class="row justify-content-center mb-5">
+    <div class="col mt-5" v-for="(item, index) of team" :key="index ">
+    <team-card v-bind:member="item"></team-card>
+    </div>
+    </div>
+    </div>
+    
+    <page-footer></page-footer>
+    
+  </div>
+</template>
+
+<script>
+
+import PageFooter from './components/PageFooter.vue';
+import TeamCard from './components/TeamCard.vue';
+import SectionApi from './components/SectionApi.vue';
+import TopMenu from './components/TopMenu.vue'
+import MainBanner from './components/MainBanner.vue';
+
+export default {
+
+
+  name: 'App',
+  components: {
+    PageFooter,
+    TeamCard,
+    SectionApi,
+    TopMenu,
+    MainBanner,
+  },
+  data () {
+    return {
+      team: [
+        {
+          codigo: 1,
+          nombre: "Amparo González",
+          descripcion: "Ingeniera civil de profesión, docente de corazón, comentarista deportiva de frustración. Hincha del buen fútbol.",
+          rol: 'Gestora de contenidos.',
+          image: './components/img/Foto-Amparo-Gonzalez-min.jpeg',
+        },
+        {
+          codigo: 2,
+          nombre: "Mario De la Pava",
+          descripcion: "Cuyabro interesado por la tecnología y el nuevo conocimiento. Biker que tiene como terapia rodar y es apasionado por viajar.",
+          rol: 'Web Master',
+          image: './components/img/Foto-Mario-De-la-Pava-min.jpeg',
+        },
+        {
+          codigo: 3,
+          nombre: "Santiago Muñoz",
+          descripcion: "Joven de 27 años, mi mayor pasatiempos es la investigación, charlas grupales, los videojuegos y el deporte, aprendiz de programación.",
+          rol: 'Dueño del Proyecto',
+          image: './components/img/Foto-Santiago-Muñoz-min.png',
+        },
+        {
+          codigo: 4,
+          nombre: "Andrés Felipe Vargas",
+          descripcion: "Trasegante por el mundo con una guitarra y una palabra, amante de los idiomas, la música y los deportes, fiel seguidor de la comida callejera.",
+          rol: 'Administrador del sitio web',
+          image: './components/img/Foto-Andres-Felipe-Vargas-min.jpg',
+        },
+      ],
+    };
+  },
+}
+</script>
+
---
+++ src/components/MainBanner.vue
@@ -0,0 +1,48 @@
+<template>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class=""></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1" class=""></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2" class=""></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item">
+            <img src='./components/img/soccer-field-drawn-on-blackboard-min.jpg' alt="" width="100%" height="400">
+            <div class="carousel-caption d-none d-md-block" style="color: #f51720;">
+                <h5>Mi Equipo</h5>
+                <p>La información de tu equipo favorito rápido y al instante.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src='./components/img/26141-min.jpg' alt="" width="100%" height="400">
+            <div class="carousel-caption d-none d-md-block" style="color: #f51720;">
+                <h5>Proximas fechas</h5>
+                <p>Toda la emoción de los siguientes partidos.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src='./components/img/vector-campo-futbol-verde-o-campo-futbol-parrilla_1284-41290-min.jpg' alt="" width="100%" height="400">
+            <div class="carousel-caption d-none d-md-block" style="color: #f51720;">
+                <h5>Staff</h5>
+                <p>Conoce a nuestro staff de miembros.</p>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'MainBanner'
+}
+</script>
